49ab45493a8f28233d8e2b58635c45414d3cd910
Andre Wruszczak <avataw.dev@gmail.com>
2021-12-16 21:54:02 +0100

    Refactor some Day 15 stuff

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -1,7 +1,5 @@
 package day15
 
-import java.util.*
-
 data class Position(val x: Int, val y: Int) {
     fun getAdjacentPositions(): List<Position> = listOf(
         Position(x, y + 1),
@@ -48,13 +46,10 @@ fun solveA(input: List<String>): Int {
 
         if (nextInQueue.chiton.position == Position(gridSize, gridSize)) return nextInQueue.distanceToStart
 
-        queue.addAll(
-            nextInQueue.chiton.adjacentChitons.filter { !it.visited }.map {
-                QueueChiton(it, nextInQueue.distanceToStart + it.riskLevel)
-            }.filter { !queue.contains(it) }
-        )
+        queue.addAll(nextInQueue.chiton.adjacentChitons.filter { !it.visited }.map {
+            QueueChiton(it, nextInQueue.distanceToStart + it.riskLevel)
+        }.filter { !queue.contains(it) })
     }
-
 
     return -1
 }
@@ -67,31 +62,20 @@ fun solveB(input: List<String>): Int {
     val inputPlusThird = input.map { it.addNumbersBy(3) }
     val inputPlusFour = input.map { it.addNumbersBy(4) }
 
-    var list = input.toMutableList()
+    val list = input.toMutableList()
 
     list.addAll(listOf(inputPlusOne, inputPlusTwo, inputPlusThird, inputPlusFour).flatten())
 
-    list = list.map {
-        it +
-                it.addNumbersBy(1) +
-                it.addNumbersBy(2) +
-                it.addNumbersBy(3) +
-                it.addNumbersBy(4)
-    }.toMutableList()
-
-    return solveA(list)
+    return solveA(list.map {
+        it + it.addNumbersBy(1) + it.addNumbersBy(2) + it.addNumbersBy(3) + it.addNumbersBy(4)
+    })
 }
 
 
-fun String.addNumbersBy(add: Int): String {
-    return this.map {
-        val number = Character.getNumericValue(it) + add
-        if (number > 9) {
-            number - 9
-        } else {
-            number
-        }
-    }.joinToString("")
-}
+
+fun String.addNumbersBy(add: Int): String = this.map {
+    val number = Character.getNumericValue(it) + add
+    if (number > 9) number - 9 else number
+}.joinToString("")
 
 
